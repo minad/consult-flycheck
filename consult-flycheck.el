0bb3272c2c957c43a568a4fb113547d3e1d39b41
Sergey Firsov <intramurz@gmail.com>
2023-06-29 00:38:31 +0300

    Fix doc comment to match coding standard

--- consult-flycheck.el
+++ consult-flycheck.el
@@ -26,7 +26,7 @@
 
 ;;; Commentary:
 
-;; Provides the command `consult-flycheck'. This is an extra package,
+;; Provides the command `consult-flycheck'.  This is an extra package,
 ;; since the consult.el package only depends on Emacs core components.
 
 ;;; Code:
